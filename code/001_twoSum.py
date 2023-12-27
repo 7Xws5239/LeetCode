e853ac7bd6afc92fc48fa9f920b1bfbe9e32ff5d
Bowman Zhu <jyhaebuhuzhi@outlook.com>
2023-12-27 09:12:33 +0800

    1

--- code/001_twoSum.py
+++ code/001_twoSum.py
@@ -2,3 +2,6 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         print()
+        for i in nums:
+            for j in nums:
+                nums[i]=nums
