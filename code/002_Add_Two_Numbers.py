3ec267451ff21c5787fb2f7d70d6ae475a937a6c
Bowman Zhu <15365687987@163.com>
2024-01-03 17:09:29 +0800

    write standard two answers of this question.

--- code/002_Add_Two_Numbers.py
+++ code/002_Add_Two_Numbers.py
@@ -6,39 +6,98 @@
 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 '''
 
-# Definition for singly-linked list.
+# # Definition for singly-linked list.
+# from typing import Optional
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+    
+#     ###################################      
+#     def __str__(self): # 用于检查NodeList内容
+#         return str(self.val)
+# class Solution:
+#     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]: # 这里的optional如果找不到的原因：没有从typing导入
+#         print()
+
+#     ###################################    
+#     def printList(self, node: Optional[ListNode]): # 用于检查NodeList内容
+#         values = []
+#         while node:
+#             values.append(str(node.val))
+#             node = node.next
+#         return '->'.join(values)
+
+# L1 = ListNode(2)
+# L1.next = ListNode(4)
+# L1.next.next = ListNode(3)
+
+
+# L2 = ListNode(5)
+# L2.next = ListNode(6)
+# L2.next.next = ListNode(4)
+
+
+# # 创建 Solution 实例并打印链表
+# sol = Solution()
+# print("L1:", sol.printList(L1))
+# print("L2:", sol.printList(L2))
+
+
+#---------------------------------------正确写法（迭代法）-------------------
 from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-    
-    ###################################      
-    def __str__(self): # 用于检查NodeList内容
-        return str(self.val)
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]: # 这里的optional如果找不到的原因：没有从typing导入
-        print()
-        
-    ###################################    
-    def printList(self, node: Optional[ListNode]): # 用于检查NodeList内容
-        values = []
-        while node:
-            values.append(str(node.val))
-            node = node.next
-        return '->'.join(values)
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        total = 0
+        next1 = 0
+        dummy = ListNode()
+        cur = dummy
+        while(l1 != None and l2 != None):
+            total = l1.val + l2.val + next1
+            cur.next = ListNode(total % 10)
+            next1 = total // 10
+            cur = cur.next
+            l1 = l1.next
+            l2 = l2.next
+        while l1 != None:
+            total = l1.val + next1
+            cur.next = ListNode(total % 10)
+            next1 = total // 10
+            cur = cur.next
+            l1 = l1.next
+        while l2 != None:
+            total = l2.val + next1
+            cur.next = ListNode(total % 10)
+            next1 = total // 10
+            cur = cur.next
+            l2 = l2.next
+        if next1 != 0:
+            cur.next = ListNode(next1)
+        return dummy.next
+#---------------------------------------正确写法-------------------
 
-L1 = ListNode(2)
-L1.next = ListNode(4)
-L1.next.next = ListNode(3)
+#---------------------------------------正确写法（递归法）-------------------
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        total = l1.val + l2.val
+        next1 = total // 10
+        res = ListNode(total % 10)
+        if (l1.next or l2.next or next1):
+            l1 = l1.next if l1.next else ListNode(0)
+            l2 = l2.next if l2.next else ListNode(0)
+            l1.val += next1
+            res.next = self.addTwoNumbers(l1,l2)
+        return res
 
 
-L2 = ListNode(5)
-L2.next = ListNode(6)
-L2.next.next = ListNode(4)
 
-
-# 创建 Solution 实例并打印链表
-sol = Solution()
-print("L1:", sol.printList(L1))
-print("L2:", sol.printList(L2))
+#---------------------------------------正确写法-------------------
