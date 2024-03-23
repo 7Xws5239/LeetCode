49095d0059cb3c2fd5a46ce49fd32c77dafdfe56
Bowman Zhu <jyhaebuhuzhi@outlook.com>
2024-03-23 15:51:43 +0800

    1

--- NewPlayerTown/Queue/622_Design_Circular_Queue.py
+++ NewPlayerTown/Queue/622_Design_Circular_Queue.py
@@ -34,42 +34,66 @@ circularQueue.Rear();  // 返回 4
 请不要使用内置的队列库。
 '''
 
+# 定义循环队列类
 class MyCircularQueue:
-
+    
+    # 循环队列的初始化方法
     def __init__(self, k: int):
-        self.k = k #我目前对这里的理解是，这个self主要是一个象征作用，主要是用于表示这个属性的作用范围的
+        # self.k 存储队列的容量
+        self.k = k
+        # 以 None 填充初始化队列，队列的大小由 k 确定
         self.queue = [None] * k
+        # 初始化头部和尾部指针为 -1，表示队列为空
         self.head = self.tail = -1
 
+    # 向队列中插入一个元素的方法
     def enQueue(self, value: int) -> bool:
+        # 检查队列是否已满，如果已满，返回 False 表示无法插入
         if self.isFull():
             return False
+        # 如果队列为空，将头部指针设置为 0
         if self.isEmpty():
             self.head = 0
+        # 循环方式将尾部指针向前移动 1 位置，并插入新的值
         self.tail = (self.tail + 1) % self.k
         self.queue[self.tail] = value
+        # 返回 True 表示值已成功插入
         return True
 
+    # 从队列中删除一个元素的方法
     def deQueue(self) -> bool:
+        # 检查队列是否为空，如果为空，返回 False 表示没有元素可以删除
         if self.isEmpty():
             return False
+        # 如果头部和尾部指针相等，表示队列中只有一个元素，将它们设置为 -1 表示队列为空
         if self.head == self.tail:
             self.head = self.tail = -1
         else:
+            # 循环方式将头部指针向前移动 1 位置，有效地移除了队列前端的元素
             self.head = (self.head + 1) % self.k
+        # 返回 True 表示成功执行了出队操作
         return True
 
+    # 获取队列前端元素的方法
     def Front(self) -> int:
+        # 如果队列为空，返回 -1；否则，返回头部指针处的值
         return -1 if self.isEmpty() else self.queue[self.head]
 
+    # 获取队列尾端元素的方法
     def Rear(self) -> int:
+        # 如果队列为空，返回 -1；否则，返回尾部指针处的值
         return -1 if self.isEmpty() else self.queue[self.tail]
 
+    # 辅助方法，用于检查队列是否为空
     def isEmpty(self) -> bool:
+        # 如果头部指针为 -1，则认为队列为空
         return self.head == -1
 
+    # 辅助方法，用于检查队列是否已满
     def isFull(self) -> bool:
+        # 如果向前移动尾部指针会使其等于头部指针，则认为队列已满
         return (self.tail + 1) % self.k == self.head
+
 
 
 
