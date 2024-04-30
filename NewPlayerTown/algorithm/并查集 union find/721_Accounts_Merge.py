295ddaa2a49e83581821a687d64059cdf108fcd8
BowmanZhu <15365687987@163.com>
2024-04-30 15:58:05 +0800

    1

--- NewPlayerTown/algorithm/并查集 union find/721_Accounts_Merge.py
+++ NewPlayerTown/algorithm/并查集 union find/721_Accounts_Merge.py
@@ -103,3 +103,49 @@ print(sol.accountsMerge(accounts))
 
 在选择最优解时，考虑问题的具体需求和实际运行环境是关键。如果性能差异不大，选择最易于实现和维护的方法也是合理的。
 '''
+
+# 官方答案（速度明显快得多，也是用了并查集）：
+
+'''
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def union(self, index1: int, index2: int):
+        self.parent[self.find(index2)] = self.find(index1)
+
+    def find(self, index: int) -> int:
+        if self.parent[index] != index:
+            self.parent[index] = self.find(self.parent[index])
+        return self.parent[index]
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        emailToIndex = dict()
+        emailToName = dict()
+
+        for account in accounts:
+            name = account[0]
+            for email in account[1:]:
+                if email not in emailToIndex:
+                    emailToIndex[email] = len(emailToIndex)
+                    emailToName[email] = name
+        
+        uf = UnionFind(len(emailToIndex))
+        for account in accounts:
+            firstIndex = emailToIndex[account[1]]
+            for email in account[2:]:
+                uf.union(firstIndex, emailToIndex[email])
+        
+        indexToEmails = collections.defaultdict(list)
+        for email, index in emailToIndex.items():
+            index = uf.find(index)
+            indexToEmails[index].append(email)
+        
+        ans = list()
+        for emails in indexToEmails.values():
+            ans.append([emailToName[emails[0]]] + sorted(emails))
+        return ans
+
+
+'''
